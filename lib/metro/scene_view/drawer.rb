817ac5cc64b676f7bb79eedf0396d646874e24cc
Franklin Webber <franklin.webber@gmail.com>
2012-10-17 23:17:37 -0400

    Updatd label color to support strings and hex fixnums

--- lib/metro/scene_view/drawer.rb
+++ lib/metro/scene_view/drawer.rb
@@ -24,10 +24,13 @@ module Metro
       def draw_label(content)
         label_text = scene.instance_eval( "\"#{content['text']}\"" )
 
+        label_color = content['color'] || 0xffffffff
+        label_color = label_color.to_i(16) if label_color.is_a? String
+
         font.draw label_text,
           content['x'], content['y'], content['z-order'],
           content['x-factor'] || 1.0, content['y-factor'] || 1.0,
-          content['color'] || 0xffffffff
+          label_color
       end
 
       def draw_options(content) ; end
