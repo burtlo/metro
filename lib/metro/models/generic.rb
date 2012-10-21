e8ff35ce8c2c0e841014309b1747ebbaab50c0d9
Franklin Webber <franklin.webber@gmail.com>
2012-10-21 19:24:40 -0400

    Generic raises helpful exception when drawing

--- lib/metro/models/generic.rb
+++ lib/metro/models/generic.rb
@@ -7,6 +7,15 @@ module Metro
     #
     class Generic < Model
 
+      def draw
+        raise cannot_draw_message
+      end
+
+      def cannot_draw_message
+        [ "Unable to draw #{name} in #{scene}",
+          "The actor named '#{name}' could not find a suitable model so it could not be drawn in the scene #{scene}" ].join("\n")
+      end
+
     end
   end
 end
