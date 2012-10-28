3b6c30af6103bc8217eb719cffc3ff033b634ce8
Franklin Webber <franklin.webber@gmail.com>
2012-10-28 03:48:26 -0400

    Label will default to center position for x and y

--- lib/metro/models/label.rb
+++ lib/metro/models/label.rb
@@ -10,20 +10,35 @@ module Metro
     class Label < Model
 
       attr_accessor :x, :y, :x_factor, :y_factor, :z_order
+      attr_accessor :size, :size, :font_family
 
       def after_initialize
         @text = ""
         @x_factor = @y_factor = 1.0
         @z_order = 0
         @color = Gosu::Color.new "rgba(255,255,255,1.0)"
+        @size = 20
+        @font_family = Gosu::default_font_name
       end
-
+      
       def font
-        @font ||= Gosu::Font.new(window, Gosu::default_font_name, 20)
+        @font ||= Gosu::Font.new(window, font_family, size)
+      end
+      
+      def x
+        @x || (Game.width/2 - font.text_width(text)/2)
+      end
+      
+      def y
+        @y || (Game.height/2 - font.height/2)
+      end
+      
+      def text
+        scene.instance_eval( "\"#{@text}\"" )
       end
 
       def draw
-        label_text = scene.instance_eval( "\"#{text}\"" )
+        label_text = text
         font.draw label_text, x, y, z_order, x_factor, y_factor, color
       end
 
