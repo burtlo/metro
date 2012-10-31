9a080bd3a32968bf1d29d7213b394a4478d79d6a
Franklin Webber <franklin.webber@gmail.com>
2012-10-31 08:56:46 -0400

    Adding generic modifier key methods

--- lib/metro/events/event_data.rb
+++ lib/metro/events/event_data.rb
@@ -21,20 +21,39 @@ module Metro
       end
     end
 
+    #
+    # TODO: This attempt to reduce duplication is brittle and will likely end in heartache.
+    # 
+
     def self.modifier_key_list_names
       @modifier_key_list_names ||= %w[ KbLeftControl KbRightControl
                                         KbLeftAlt KbRightAlt
                                         KbLeftMeta KbRightMeta
                                         KbLeftShift KbRightShift ]
     end
-    
+
     def self.modifier_key_list
       @modifier_key_list ||= modifier_key_list_names.map {|key| "Gosu::#{key}".constantize }
     end
 
+    #
+    # Generate methods for each left and right modifier key
+    #
+
     modifier_key_list_names.each_with_index do |key_name,index|
       define_method "#{key_name.gsub(/^Kb/,'').underscore}?" do
         modifier_keys[self.class.modifier_key_list[index]]
+      end
+    end
+
+    #
+    # Define generic modifier keys that is not concerned with whether it
+    # was the left key or the right key.
+    #
+
+    [ :control?, :alt?, :meta?, :shift? ].each do |generic|
+      define_method generic do
+        send("left_#{generic}") or send("right_#{generic}")
       end
     end
 
