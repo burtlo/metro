0393d18832fe78a0baf1d36d0c0309af2d7cac4f
Franklin Webber <franklin.webber@gmail.com>
2012-11-05 13:22:20 -0500

    Template will automatically add reload with Ctrl+R

--- lib/templates/game/scenes/game_scene.rb
+++ lib/templates/game/scenes/game_scene.rb
@@ -2,16 +2,18 @@ class GameScene < Metro::Scene
 
   #
   # The game scene is a place where you can define actors and
-  # events here that will be present within all the sub-classed
+  # events here that will be present within all the subclassed
   # scenes.
+
   #
   # @example Setting up the ability for all subclassed scenes
-  #   to be reloaded with the 'R' event
+  #   to be reloaded with the 'Ctrl+R' event
   #
-  # event :on_up, KbR do
-  #   Metro.reload!
-  #   transition_to self.class.scene_name
-  # end
-  #
+  event :on_up, KbR do |event|
+    if event.control?
+      Metro.reload!
+      transition_to self.class.scene_name
+    end
+  end
 
 end
