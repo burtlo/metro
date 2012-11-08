fd1e2d44f893df54906185a342e47615d1c3b071
Franklin Webber <franklin.webber@gmail.com>
2012-11-08 11:36:46 -0500

    FIX parse called wrong method

--- lib/metro/units/dimensions.rb
+++ lib/metro/units/dimensions.rb
@@ -3,15 +3,15 @@ module Metro
     class Dimensions < Struct.new(:width,:height)
 
       def self.none
-        new 0.0, 0.0
+        of 0.0, 0.0
+      end
+
+      def self.parse(string)
+        of *string.split(",",2).map(&:to_f)
       end
 
       def self.of(width,height)
         new width.to_f, height.to_f
-      end
-
-      def self.parse(string)
-        to *string.split(",",2).map(&:to_f)
       end
 
       def to_s
