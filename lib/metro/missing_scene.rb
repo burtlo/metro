b500a3932735604bb8a9524bd3b545c930617b00
Franklin Webber <franklin.webber@gmail.com>
2012-11-11 01:25:00 -0500

    Missing scene is now in RED and shipped in the template

--- lib/metro/missing_scene.rb
+++ lib/metro/missing_scene.rb
@@ -8,13 +8,13 @@ module Metro
 
     draw :title, text: "Missing Scene!",
       position: "20,20,1",
-      color: 0xffffffff,
-      font: {size: 40},
+      color: "rgb(255,0,0)",
+      font: {size: 80},
       model: "metro::models::label"
-
+    
     draw :message, text: 'The scene `#{self.class.missing_scene}` was requested, but is missing!',
       position: "20,100,1",
-      color: 0xffffffff,
+      color: "rgb(255,255,255)",
       model: "metro::models::label"
 
   end
