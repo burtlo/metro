921bb59d1344e9ec626fdc57352757cac6732976
Franklin Webber <franklin.webber@gmail.com>
2012-11-18 17:08:40 -0500

    Generic Model - cannot draw message
    Instead of raising an exception with an error message an warning is
    displayed within the scene with the details about the model that was
    attempted to be displayed.

--- lib/metro/models/ui/generic.rb
+++ lib/metro/models/ui/generic.rb
@@ -2,18 +2,21 @@ module Metro
   module UI
 
     #
-    # Generic model is used when no model is cannot not be found
-    # when mapping the view content
+    # Generic model is used when no model can be found.
     #
     class Generic < Model
 
+      attr_accessor :warned
+
       def draw
-        raise cannot_draw_message
+        log.warn cannot_draw_message unless warned
+        self.warned = true
       end
 
       def cannot_draw_message
-        [ "Unable to draw #{name} in #{scene}",
-          "The actor named '#{name}' could not find a suitable model so it could not be drawn in the scene #{scene}" ].join("\n")
+        [ "Unable to draw #{name} in #{scene}", "",
+          "  The actor named '#{name}' does not specify a suitable model so it could not be drawn in the scene.",
+          "", "  " + properties.to_s, "" ].join("\n")
       end
 
     end
