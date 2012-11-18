b6d375e168e9e76c3d48c4eb1f84ae078b648f48
Franklin Webber <franklin.webber@gmail.com>
2012-11-17 19:00:41 -0500

    When a model is being loaded it will try and use the property setters/getters before falling back to storing the property

--- lib/metro/models/model.rb
+++ lib/metro/models/model.rb
@@ -273,7 +273,13 @@ module Metro
 
       # Clean up and symbolize all the keys then merge  that with the existing properties
       options.keys.each do |key|
-        options[key.to_s.underscore.to_sym] = options.delete(key)
+        property_name = key.to_s.underscore.to_sym
+        if respond_to? "#{property_name}="
+          send("#{property_name}=",options.delete(key))
+        else
+          options[property_name] = options.delete(key)
+        end
+        
       end
 
       properties.merge! options
