fdd3ba910b17900a03d04e384e3fa7afbf5efe1e
Franklin Webber <franklin.webber@gmail.com>
2012-12-10 21:19:09 -0600

    Refactored and Stub Viewport code

--- lib/metro/models/ui/tile_map.rb
+++ lib/metro/models/ui/tile_map.rb
@@ -1,7 +1,7 @@
 module Metro
   module UI
 
-    class TileMapOrthogonalLayer < Model
+    class TileLayer < Model
       property :position
       property :rotation
 
@@ -13,6 +13,12 @@ module Metro
         layer.data
       end
 
+      def viewport
+        @viewport ||= begin
+          b = Bounds.new left: 0 - map.tilewidth, right: 640 + map.tilewidth, top: 0 - map.tileheight * 2, bottom: 480 + map.tileheight
+        end
+      end
+
       def draw
         data.each_with_index do |image_index,position_index|
           next if image_index == 0
@@ -21,10 +27,11 @@ module Metro
           draw_image(image_index,row,column)
         end
       end
-      
+
       def draw_image(image_index,row,column)
         image = image_from_tileset(image_index)
         point = image_point(image,row,column)
+        return unless viewport.contains?(point)
         image.draw_rot(point.x,point.y,z_order,rotation)
       end
 
@@ -54,23 +61,20 @@ module Metro
       def cached_positions
         @cached_positions ||= Hash.new {|hash,key| hash[key] = {} }
       end
-      
-
     end
 
-    class TileMapIsometricLayer < Model
-
-      property :position
-      property :rotation
-
-      attr_accessor :map
-      attr_accessor :layer
-      attr_accessor :tilesets
-
-      def data
-        layer.data
+    class TileMapOrthogonalLayer < TileLayer
+      def image_point(image,row,column)
+        unless cached_positions[row][column]
+          pos_x = x + column * map.tilewidth
+          pos_y = y + row * map.tileheight
+          cached_positions[row][column] = Units::Point.at(pos_x, pos_y)
+        end
+        cached_positions[row][column]
       end
+    end
 
+    class TileMapIsometricLayer < TileLayer
       def half_tilewidth
         @half_tilewidth ||= map.tilewidth/2
       end
@@ -97,35 +101,6 @@ module Metro
         row_start_y + half_tileheight * column
       end
 
-      def draw
-        data.each_with_index do |image_index,position_index|
-          next if image_index == 0
-          column = position_index % layer.width
-          row = position_index / layer.height
-          draw_image(image_index,row,column)
-        end
-      end
-
-      def draw_image(image_index,row,column)
-        image = image_from_tileset(image_index)
-        point = image_point(image,row,column)
-        image.draw_rot(point.x,point.y,z_order,rotation)
-      end
-
-      def image_from_tileset(image_index)
-        unless cached_images[image_index]
-          tileset = map.tilesets.find {|t| image_index >= t.firstgid && image_index < t.firstgid + t.images.count }
-          tileset_image_index = image_index - tileset.firstgid
-          cached_images[image_index] = tileset.images[tileset_image_index]
-        end
-
-        cached_images[image_index]
-      end
-
-      def cached_images
-        @cached_images ||= {}
-      end
-
       def image_point(image,row,column)
         unless cached_positions[row][column]
           pos_x = x_position(row,column) - (map.tilewidth - image.width)/2
@@ -133,10 +108,6 @@ module Metro
           cached_positions[row][column] = Units::Point.at(pos_x, pos_y)
         end
         cached_positions[row][column]
-      end
-
-      def cached_positions
-        @cached_positions ||= Hash.new {|hash,key| hash[key] = {} }
       end
     end
 
@@ -164,7 +135,6 @@ module Metro
 
       def show
         self.layers = map.layers.collect do |layer|
-
           tml = layer_class.new
           tml.position = position
           tml.rotation = rotation
@@ -173,6 +143,10 @@ module Metro
           tml.tilesets = map.tilesets
           tml
         end
+      end
+
+      def update
+        # update the layer with the current viewport position
       end
 
       def draw
