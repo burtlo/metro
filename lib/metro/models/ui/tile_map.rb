d011aa1103cfa1a16e89c622116e8c9421b7583c
Franklin Webber <franklin.webber@gmail.com>
2012-12-10 20:26:02 -0600

    Support for both orthogonal and isometric

--- lib/metro/models/ui/tile_map.rb
+++ lib/metro/models/ui/tile_map.rb
@@ -1,50 +1,31 @@
 module Metro
   module UI
 
-    class TileMap < Model
-
+    class TileMapOrthogonalLayer < Model
       property :position
-      property :file, type: :text
       property :rotation
 
-      def map
-        @map ||= begin
-          map = Tmxed.parse asset_path(file)
-          map.tilesets.each {|tileset| tileset.window = window }
-          map
-        end
+      attr_accessor :map
+      attr_accessor :layer
+      attr_accessor :tilesets
+
+      def data
+        layer.data
       end
 
       def draw
-        map.layers.each { |layer| draw_layer(layer) }
+        data.each_with_index do |image_index,position_index|
+          next if image_index == 0
+          column = position_index % layer.width
+          row = position_index / layer.height
+          draw_image(image_index,row,column)
+        end
       end
-
-      private
-
-      def half_tilewidth
-        @half_tilewidth ||= map.tilewidth/2
-      end
-
-      def half_tileheight
-        @half_tileheight ||= map.tileheight/2
-      end
-
-      def start_x
-        @start_x ||= x + map.tilewidth * map.width / 2
-      end
-
-      def x_position(row,column,image_width)
-        row_start_x = start_x - half_tilewidth * row - (map.tilewidth - image_width)/2
-        row_start_x + half_tilewidth * column
-      end
-
-      def start_y
-        y
-      end
-
-      def y_position(row,column,image_height)
-        row_start_y = start_y + half_tileheight * row + (map.tileheight - image_height)/2
-        row_start_y + half_tileheight * column
+      
+      def draw_image(image_index,row,column)
+        image = image_from_tileset(image_index)
+        point = image_point(image,row,column)
+        image.draw_rot(point.x,point.y,z_order,rotation)
       end
 
       def image_from_tileset(image_index)
@@ -61,8 +42,63 @@ module Metro
         @cached_images ||= {}
       end
 
-      def draw_layer(layer)
-        layer.data.each_with_index do |image_index,position_index|
+      def image_point(image,row,column)
+        unless cached_positions[row][column]
+          pos_x = x + column * map.tilewidth
+          pos_y = y + row * map.tileheight
+          cached_positions[row][column] = Units::Point.at(pos_x, pos_y)
+        end
+        cached_positions[row][column]
+      end
+
+      def cached_positions
+        @cached_positions ||= Hash.new {|hash,key| hash[key] = {} }
+      end
+      
+
+    end
+
+    class TileMapIsometricLayer < Model
+
+      property :position
+      property :rotation
+
+      attr_accessor :map
+      attr_accessor :layer
+      attr_accessor :tilesets
+
+      def data
+        layer.data
+      end
+
+      def half_tilewidth
+        @half_tilewidth ||= map.tilewidth/2
+      end
+
+      def half_tileheight
+        @half_tileheight ||= map.tileheight/2
+      end
+
+      def start_x
+        @start_x ||= x + map.tilewidth * map.width / 2
+      end
+
+      def x_position(row,column)
+        row_start_x = start_x - half_tilewidth * row
+        row_start_x + half_tilewidth * column
+      end
+
+      def start_y
+        y
+      end
+
+      def y_position(row,column)
+        row_start_y = start_y + half_tileheight * row
+        row_start_y + half_tileheight * column
+      end
+
+      def draw
+        data.each_with_index do |image_index,position_index|
           next if image_index == 0
           column = position_index % layer.width
           row = position_index / layer.height
@@ -76,23 +112,72 @@ module Metro
         image.draw_rot(point.x,point.y,z_order,rotation)
       end
 
+      def image_from_tileset(image_index)
+        unless cached_images[image_index]
+          tileset = map.tilesets.find {|t| image_index >= t.firstgid && image_index < t.firstgid + t.images.count }
+          tileset_image_index = image_index - tileset.firstgid
+          cached_images[image_index] = tileset.images[tileset_image_index]
+        end
+
+        cached_images[image_index]
+      end
+
+      def cached_images
+        @cached_images ||= {}
+      end
+
       def image_point(image,row,column)
         unless cached_positions[row][column]
-          pos_x = x_position(row,column,image.width)
-          pos_y = y_position(row,column,image.height)
-          cached_positions[row][column] = Point.at(pos_x, pos_y)
+          pos_x = x_position(row,column) - (map.tilewidth - image.width)/2
+          pos_y = y_position(row,column) + (map.tileheight - image.height)/2
+          cached_positions[row][column] = Units::Point.at(pos_x, pos_y)
         end
         cached_positions[row][column]
       end
 
       def cached_positions
-        @cached_positions ||= begin
-          Hash.new do |hash,key|
-            hash[key] = {}
-          end
+        @cached_positions ||= Hash.new {|hash,key| hash[key] = {} }
+      end
+    end
+
+
+    class TileMap < Model
+
+      property :position
+      property :file, type: :text
+      property :rotation
+
+      attr_accessor :layers
+
+      def map
+        @map ||= begin
+          map = Tmxed.parse asset_path(file)
+          map.tilesets.each {|tileset| tileset.window = window }
+          map
         end
       end
 
+      def layer_class
+        { orthogonal: "Metro::UI::TileMapOrthogonalLayer",
+          isometric: "Metro::UI::TileMapIsometricLayer" }[map.orientation.to_sym].constantize
+      end
+
+      def show
+        self.layers = map.layers.collect do |layer|
+
+          tml = layer_class.new
+          tml.position = position
+          tml.rotation = rotation
+          tml.map = map
+          tml.layer = layer
+          tml.tilesets = map.tilesets
+          tml
+        end
+      end
+
+      def draw
+        layers.each {|layer| layer.draw }
+      end
     end
   end
 end
--- lib/tmxed_ext/tile_set.rb
+++ lib/tmxed_ext/tile_set.rb
@@ -2,8 +2,38 @@ module Tmxed
   class TileSet
     attr_accessor :window
 
+    def full_width
+      tilewidth + spacing
+    end
+
+    def full_height
+      tileheight + spacing
+    end
+
+    def crop_bounds
+      [ margin, margin, tilewidth - spacing, tileheight - spacing ]
+    end
+
+    def image_path
+      Metro::AssetPath.with(image).filepath
+    end
+
     def images
-      @images ||= Gosu::Image.load_tiles(window,Metro::AssetPath.with(image).filepath,tilewidth,tileheight,true)
+      @images ||= begin
+        original_images = Gosu::Image.load_tiles window, image_path, full_width,full_height, true
+        crop_images original_images
+      end
+    end
+
+    private
+
+    def crop_images(images)
+      return images if spacing > 0
+      images.map do |image|
+        cropped_image = TexPlay.create_image(window,tilewidth,tileheight)
+        cropped_image.splice image, 0, 0, crop: crop_bounds
+        cropped_image
+      end
     end
   end
 end
