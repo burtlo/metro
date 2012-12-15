f21eac76ad4989960ddc19b591e4247f25b919af
Franklin Webber <franklin.webber@gmail.com>
2012-12-15 10:57:02 -0600

    right,bottom bounds correctly determined

--- lib/metro/models/ui/tile_map.rb
+++ lib/metro/models/ui/tile_map.rb
@@ -77,7 +77,7 @@ module Metro
       def position_of_image(image,row,column)
         pos_x = x + column * map.tilewidth + map.tilewidth / 2
         pos_y = y + row * map.tileheight + map.tileheight / 2
-        Bounds.new left: pos_x, top: pos_y, right: pos_x + map.tilewidth, bottom: pos_y + map.tileheight
+        Bounds.new left: pos_x, top: pos_y, right: pos_x + map.tilewidth, bottom: pos_y + map.tileheight/2
       end
     end
 
